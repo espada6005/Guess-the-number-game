789c952dca899c3f83fb1c7b2198d2985f8192fd
espada <yua2479@gmail.com>
2024-12-01 14:39:25 +0900

    add main.py update README.md

---
+++ main.py
@@ -0,0 +1,23 @@
+import random
+
+while True:
+    min_num = int(input("最小値を入力してください："))
+    max_num = int(input("最大値を入力してください："))
+    if min_num < max_num:
+        break
+    else:
+        print("\n最小値 < 最大値になるように入力してください\n")
+
+answer = random.randint(min_num, max_num)
+print(answer)
+
+guess_num = int(input("数字を入力してください："))
+
+while guess_num != answer:
+    if guess_num < answer:
+        print(f"答えは{guess_num}より大きいです")
+    else:
+        print(f"答えは{guess_num}より小さいです")
+    guess_num = int(input("数字を入力してください："))
+
+print("正解です")
